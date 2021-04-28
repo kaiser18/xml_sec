76926750d7e1cea05fab680bb7dee66f279c5893
Mihailo Ivić <oliahim.ivic@gmail.com>
2021-04-28 21:36:03 +0200

    fixed database annotation for confirmation token

--- bezbednost/src/main/java/bezbednost/domain/ConfirmationToken.java
+++ bezbednost/src/main/java/bezbednost/domain/ConfirmationToken.java
@@ -10,8 +10,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -32,7 +31,7 @@ public class ConfirmationToken {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createdDate;
 	
-	@OneToMany(targetEntity = User.class, fetch = FetchType.EAGER)
+	@ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
 	private User user;
 	
 	private Date expiryDate;
