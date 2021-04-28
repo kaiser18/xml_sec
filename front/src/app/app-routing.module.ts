51c783d58926d97156e450e8649ceb0d240fb20d
Jovan Timarac <don.quinzel@gmail.com>
2021-04-28 13:04:12 +0200

    #interceptor & guard implementation

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { CertificateListComponent } from './certificate-list/certificate-list.component';
 import { CreateCertificateComponent } from './create-certificate/create-certificate.component';
 import { UserCertificatesComponent } from './user-certificates/user-certificates.component';
+import { LoginComponent } from './login_logistics/login/login.component';
+import { HomeComponent } from './login_logistics/home/home.component';
+import { MainHomepageComponent } from './login_logistics/main-homepage/main-homepage.component';
+import { AuthGuard } from '../app/login_logistics/_helpers';
 
 const routes: Routes = [
   {
@@ -18,6 +22,21 @@ const routes: Routes = [
   {
     path: "certificateList",
     component: CertificateListComponent
+  },
+
+  {
+    path: "logIn",
+    component: LoginComponent
+  },
+
+    {
+      path: "HomeX",
+      component: MainHomepageComponent
+    },
+
+  {
+    path: "Home",
+    component: HomeComponent, canActivate: [AuthGuard]
   }
 
 ];
