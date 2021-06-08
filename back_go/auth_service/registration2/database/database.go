8e1f4611ca1dd3157bb338229252bdf2433189fe
Mihailo Ivić <oliahim.ivic@gmail.com>
2021-06-08 15:49:38 +0200

    switched to docker databases

--- back_go/auth_service/registration2/database/database.go
+++ back_go/auth_service/registration2/database/database.go
@@ -1,12 +1,13 @@
 package database
 
 import (
-    "back_go/auth_service/registration2/helpers"
+	"back_go/auth_service/registration2/helpers"
+
 	"github.com/jinzhu/gorm"
 
-    // "gorm.io/gorm"
-    // "gorm.io/driver/postgres"
-    _ "github.com/lib/pq"
+	// "gorm.io/gorm"
+	// "gorm.io/driver/postgres"
+	_ "github.com/lib/pq"
 )
 
 // Create global variable
@@ -15,8 +16,8 @@ var DB *gorm.DB
 // Create InitDatabase function
 func InitDatabase() {
 
-    //database, err := gorm.Open("postgres", "host=db port=5432 user=postgres dbname=users password=postgres sslmode=disable")
-    database, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=mrdocker password=petarpanizvoncica sslmode=disable")
+	database, err := gorm.Open("postgres", "host=db port=5432 user=postgres dbname=users password=postgres sslmode=disable")
+	//database, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=mrdocker password=petarpanizvoncica sslmode=disable")
 
 	helpers.HandleErr(err)
 	// Set up connection pool
